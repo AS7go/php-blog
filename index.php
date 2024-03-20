728acbcc5c3a83fc77efbf5bb1e58adde845a26f
Aleksandr <sh17aleksandr@gmail.com>
2024-03-20 05:54:22 +0200

    add url base_url asset_url

--- index.php
+++ index.php
@@ -1,12 +1,15 @@
 <?php
 
 use Blog\LatestPosts;
+use Blog\Slim\TwigMiddleware;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
 use Blog\PostMapper;
+
+//===============
 
 require __DIR__ . '/vendor/autoload.php';
 
@@ -27,10 +30,10 @@ try {
     die();
 }
 
-// $postMapper = new PostMapper($connection);
-
 // Create app
 $app = AppFactory::create();
+
+$app->add(new TwigMiddleware($view));
 
 $app->get('/', function (Request $request, Response $response) use ($view, $connection) {
     $latestPosts = new LatestPosts($connection);
